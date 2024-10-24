6f5f25d543a4a5919da1987eba298fd2a99c9f54
PhilipQuirke <philipquirkenz@gmail.com>
2024-10-25 10:00:30 +1300

    Initial setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='QuantaMechInterp',
+    version='1.1',
+    packages=find_packages(),
+    description='Tools for use with the quanta framework and transformer models',
+    author='Philip Quirke et al',
+    author_email='philipquirkenzgmail.com',
+    install_requires=[
+        'numpy>=1.18.1',
+        'wheel'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
